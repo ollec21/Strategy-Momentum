75d466d865d259698a32418ffb60d330791219b5
kenorb <kenorb@users.noreply.github.com>
2021-01-16 17:31:15 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -6,8 +6,8 @@
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_Momentum_Params_M15 : MomentumParams {
   Indi_Momentum_Params_M15() : MomentumParams(indi_momentum_defaults, PERIOD_M15) {
-    applied_price = (ENUM_APPLIED_PRICE)3;
-    period = 12;
+    applied_price = (ENUM_APPLIED_PRICE)1;
+    period = 16;
     shift = 0;
   }
 } indi_momentum_m15;
@@ -19,12 +19,12 @@ struct Stg_Momentum_Params_M15 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
